6f499c0a9a74a1f0f3f4aba36d7271d1e7f510a5
root <xtoniox@hotmail.fr>
2019-07-01 15:03:41 +0200

    fix

--- app.js
+++ app.js
@@ -24,30 +24,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//  app.use(function(req, res, next) {
-//    if ((req.get('X-Forwarded-Proto') !== 'https')) {
-//      res.redirect('https://' + req.get('Host') + req.url);
-//    } else
-//      next();
-//  });
+app.use(function (req, res, next) {
+  if ((req.get('X-Forwarded-Proto') !== 'https')) {
+    res.redirect('https://' + req.get('Host') + req.url);
+  } else
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-        next(createError(404));
-      });
-      
-      // error handler
-      app.use(function(err, req, res, next) {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-      
-        // render the error page
-        res.status(err.status || 500);
-        res.render('404');
-      });
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('404');
+});
 
 module.exports = app;
