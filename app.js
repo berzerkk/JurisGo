75ab52c612f909a41adf4aacc1daa1fe26affa1d
root <xtoniox@hotmail.fr>
2019-07-17 13:43:11 +0200

    fix

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(function (req, res, next) {
-//   if ((req.get('X-Forwarded-Proto') !== 'https')) {
-//     res.redirect('https://' + req.get('Host') + req.url);
-//   } else
-//     next();
-// });
+app.use(function (req, res, next) {
+  if ((req.get('X-Forwarded-Proto') !== 'https')) {
+    res.redirect('https://' + req.get('Host') + req.url);
+  } else
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
